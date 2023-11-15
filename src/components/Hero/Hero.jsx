181c3c9e16eb73c02b375c7ebf3d4f3affbd2040
Avijit Samanta <avijit.tict@gmail.com>
2023-11-16 00:22:54 +0530

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className={styles.hero}>
         <div className={styles.hero_content_wrapper}>
-            <div className={styles.text_div}>100 Thousand Songs,ad-free</div>
+            <div className={styles.text_div}><div>100 Thousand Songs,ad-free</div><div>Over thousands podcast episodes</div></div>
             <img src={HeadphoneImage} alt='headphone' width={212} height={212}/>
         </div>
     </div>
