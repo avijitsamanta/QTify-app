e8b355fb663d9622a09a4997fbc05bd56ad14a68
Avijit Samanta <avijit.tict@gmail.com>
2023-11-15 23:20:56 +0530

    hero section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Button from './components/Button';
 import SearchBar from './components/SearchBar';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Hero.module.css'
+import HeadphoneImage from '../../assets/vibrating-headphone.png'
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+        <div className={styles.hero_content_wrapper}>
+            <div className={styles.text_div}><p>100 Thousand Songs,ad-free</p><p>Over thousands podcast episodes</p></div>
+            <img src={HeadphoneImage} alt='headphone' width={212} height={212}/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
