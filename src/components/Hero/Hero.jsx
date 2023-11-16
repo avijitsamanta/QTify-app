a98e550e7afdd6d3bb1fcc75585dc1ccb33505ed
Avijit Samanta <avijit.tict@gmail.com>
2023-11-16 09:47:22 +0530

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,10 @@ import HeadphoneImage from '../../assets/vibrating-headphone.png'
 function Hero() {
   return (
     <div className={styles.hero}>
-        <p>100 Thousand Songs,ad-free.</p>
-        <p>Over thousands podcast episodes</p>
-        <img src={HeadphoneImage} alt='headphone' width={212} height={212}/>
+        <div className={styles.hero_content_wrapper}>
+            <div className={styles.text_div}><p>100 Thousand Songs,ad-free</p><p>Over thousands podcast episodes</p></div>
+            <img src={HeadphoneImage} alt='headphone' width={212} height={212}/>
+        </div>
     </div>
   )
 }
